f64cb06104e7523da819102893d6e906baf569ba
HerikWillian <166548471+HerikWillian@users.noreply.github.com>
2024-10-06 00:17:31 +0000

    responsivo

--- animacoes.js
+++ animacoes.js
@@ -17,24 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const observer = new IntersectionObserver(observerCallback, observerOptions);
     const targets = document.querySelectorAll(".contact-item, .aspectos");
-    
+
     targets.forEach(target => {
         observer.observe(target);
     });
 
-    const changeBackground = (event) => {
-        const colors = ['#ffcccb', '#add8e6', '#90ee90', '#f0e68c', '#ffb6c1'];
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
-        event.currentTarget.style.backgroundColor = randomColor;
-    };
-
-    const showMessage = (event) => {
-        event.preventDefault();
-        alert('Você clicou em: ' + event.currentTarget.innerText);
-    };
-
-    targets.forEach(item => {
-        item.addEventListener('click', changeBackground);
-        item.addEventListener('click', showMessage);
-    });
+    // Remove a função changeBackground e seus eventos
 });
